95d3d20f9bcdc43d5e8e1c5897737d60ccf22839
cooksey3 <chris@cookseys.org>
2018-01-26 13:43:23 -0500

    Test shouldDecreaseBoredomLevel created

--- src/main/java/virtual/pet/VirtualPet.java
+++ src/main/java/virtual/pet/VirtualPet.java
@@ -2,10 +2,43 @@ package virtual.pet;
 
 public class VirtualPet {
 
+	private int hungerLevel;
 	private String petName = "Bilbo";
-	
-	public boolean AllowAccess(String enteredPetName) {
+	private int thirstLevel;
+	private int boredomLevel;
+
+	public VirtualPet() {
+		hungerLevel = 1;
+		boredomLevel = 1;
+		thirstLevel = 1;
+	}
+
+	public boolean callPet(String enteredPetName) {
 		return enteredPetName.equals(petName);
+	}
+
+	public int getHungerLevel() {
+		return hungerLevel;
+	}
+
+	public int getThirstLevel() {
+		return thirstLevel;
+	}
+
+	public int getBoredomLevel() {
+		return boredomLevel;
+	}
+
+	public int getDecreasedThirstLevel() {
+		return thirstLevel -= 1;
+	}
+
+	public int getDecreasedHungerLevel() {
+		return hungerLevel -= 1;
+	}
+
+	public int getDecreasedBoredomLevel() {
+		return boredomLevel -= 1;
 	}
 
 }
--- src/test/java/virtual/pet/VirtualPetTest.java
+++ src/test/java/virtual/pet/VirtualPetTest.java
@@ -4,15 +4,64 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class VirtualPetTest {
-	
+
 	@Test
 	public void allowInteractionByPetName() {
 		VirtualPet underTest = new VirtualPet();
-		boolean check = underTest.AllowAccess("Bilbo");
+		boolean check = underTest.callPet("Bilbo");
 		Assert.assertTrue(check);
-		
-		
-		
+	}
+
+	@Test
+	public void shouldHaveDefaultHungerLevel() {
+		VirtualPet underTest = new VirtualPet();
+		int hungerLevel = underTest.getHungerLevel();
+		Assert.assertEquals(1, hungerLevel);
+	}
+
+	@Test
+	public void shouldHaveDefaultThirstLevel() {
+		VirtualPet underTest = new VirtualPet();
+		int thirstLevel = underTest.getThirstLevel();
+		Assert.assertEquals(1, thirstLevel);
+	}
+
+	@Test
+	public void shouldHaveDefaultBoredomLevel() {
+		VirtualPet underTest = new VirtualPet();
+		int boredomLevel = underTest.getBoredomLevel();
+		Assert.assertEquals(1, boredomLevel);
+	}
+
+	@Test
+	public void shouldDecreaseThirstLevel() {
+		VirtualPet underTest = new VirtualPet();
+		int thirstLevel = underTest.getDecreasedThirstLevel();
+		int decreasedThirstLevel = thirstLevel - 1;
+		Assert.assertEquals(0, thirstLevel);
+	}
+
+	@Test
+	public void shouldDecreaseHungerLevel() {
+		VirtualPet underTest = new VirtualPet();
+		int hungerLevel = underTest.getDecreasedHungerLevel();
+		int decreasedHungerLevel = hungerLevel - 1;
+		Assert.assertEquals(0, hungerLevel);
+	}
+
+	@Test
+	public void shouldDecreaseBoredomLevel() {
+		VirtualPet underTest = new VirtualPet();
+		int boredomLevel = underTest.getDecreasedBoredomLevel();
+		int decreasedBoredomLevel = boredomLevel - 1;
+		Assert.assertEquals(0, boredomLevel);
 	}
 	
+	@Test
+	public void tickHunger() {
+		VirtualPet underTest = new VirtualPet();
+		int hungerLevel = underTest.tick().hunger();
+		int tick = 
+	}
+
 }
