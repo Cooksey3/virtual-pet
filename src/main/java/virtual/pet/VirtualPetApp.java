79877acdf237b576a10a6fd1e9b01bd068ea0a11
cooksey3 <chris@cookseys.org>
2018-01-28 14:36:45 -0500

    Simplified duplicate System.out.print lines

--- src/main/java/virtual/pet/VirtualPetApp.java
+++ src/main/java/virtual/pet/VirtualPetApp.java
@@ -12,22 +12,23 @@ public class VirtualPetApp {
 		String petName = input.next();
 
 		VirtualPet myPet = new VirtualPet(petName);
-
-		System.out.println(petName + "'s vitals are:");
-		System.out.println("Hunger: " + myPet.getHungerLevel());
-		System.out.println("Thirst: " + myPet.getThirstLevel());
-		System.out.println("Boredom: " + myPet.getBoredomLevel());
-		System.out.println();
-		System.out.println("What would you like to do?");
-		System.out.println("0: Give " + petName + " up for adoption.");
-		System.out.println("1: Feed " + petName + ".");
-		System.out.println("2: Give water to " + petName + ".");
-		System.out.println("3: Play with " + petName + ".");
-		System.out.println("4: Put  " + petName + " down for a nap.");
-		System.out.print("Select an option: ");
-		String option = input.next();
+		String option = "";
 
 		while (!option.equals("0")) {
+
+			System.out.println(petName + "'s vitals are:");
+			System.out.println("Hunger: " + myPet.getHungerLevel());                                                    
+			System.out.println("Thirst: " + myPet.getThirstLevel());
+			System.out.println("Boredom: " + myPet.getBoredomLevel());
+			System.out.println();
+			System.out.println("What would you like to do?");
+			System.out.println("0: Give " + petName + " up for adoption.");
+			System.out.println("1: Feed " + petName + ".");
+			System.out.println("2: Give water to " + petName + ".");
+			System.out.println("3: Play with " + petName + ".");
+			System.out.println("4: Put  " + petName + " down for a nap.");
+			System.out.print("Select an option: ");
+			option = input.next();
 
 			if (option.equals("1")) {
 				myPet.decreaseHungerLevel();
@@ -38,26 +39,26 @@ public class VirtualPetApp {
 			} else if (option.equals("3")) {
 				myPet.decreaseBoredomLevel();
 				System.out.println("You played with " + petName + ".");
-			} else {
+			} else if (!option.equals("0")) {
 				System.out.println("Choose a different option");
 			}
 
 			myPet.tick();
 
-			System.out.println();
-			System.out.println(myPet + "'s vitals are now:");
-			System.out.println("Hunger: " + myPet.getHungerLevel());
-			System.out.println("Thirst: " + myPet.getThirstLevel());
-			System.out.println("Boredom: " + myPet.getBoredomLevel());
-			System.out.println();
-			System.out.println("What else would you like to do?");
-			System.out.println("0: Give " + petName + " up for adoption.");
-			System.out.println("1: Feed " + petName + ".");
-			System.out.println("2: Give water to " + petName + ".");
-			System.out.println("3: Play with " + petName + ".");
-			System.out.println("4: Put  " + petName + " down for a nap.");
+//			System.out.println();
+//			System.out.println(myPet + "'s vitals are now:");
+//			System.out.println("Hunger: " + myPet.getHungerLevel());
+//			System.out.println("Thirst: " + myPet.getThirstLevel());
+//			System.out.println("Boredom: " + myPet.getBoredomLevel());
+//			System.out.println();
+//			System.out.println("What else would you like to do?");
+//			System.out.println("0: Give " + petName + " up for adoption.");
+//			System.out.println("1: Feed " + petName + ".");
+//			System.out.println("2: Give water to " + petName + ".");
+//			System.out.println("3: Play with " + petName + ".");
+//			System.out.println("4: Put  " + petName + " down for a nap.");
 
-			option = input.next();
+//			option = input.next();
 		}
 
 		System.out.println("You gave " + petName + " up for adoption. " + petName + " is heartbroken.");
